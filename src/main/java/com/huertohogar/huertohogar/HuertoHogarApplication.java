4b8e3e908ff1b239010196008a932dad48d2282b
maaurissio <maur.gajardo@duocuc.cl>
2025-11-30 20:54:49 -0300

    Creacion back

---
+++ src/main/java/com/huertohogar/huertohogar/HuertoHogarApplication.java
@@ -0,0 +1,13 @@
+package com.huertohogar.huertohogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertoHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertoHogarApplication.class, args);
+	}
+
+}
